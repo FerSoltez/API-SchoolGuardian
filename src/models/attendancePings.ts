f57c2c7468f0c8a937a9f50eb1817e37a8e7b56c
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-07-28 21:37:15 -0600

    Act

--- src/models/attendancePings.ts
+++ src/models/attendancePings.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model, Optional } from "sequelize";
-import sequelize from "../config/database";
+import { sequelize } from "../config/database";
 
 // Definir la interfaz para los atributos del modelo
 interface AttendancePingsAttributes {
